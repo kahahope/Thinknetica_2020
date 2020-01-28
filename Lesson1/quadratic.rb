8aba536bdc52213e50f6eb4f568f3e795984c0ee
kahahope <kahahope@gmail.com>
2020-01-28 04:46:05 +0300

    Tab changes

--- Lesson1/quadratic.rb
+++ Lesson1/quadratic.rb
@@ -10,13 +10,13 @@ c = gets.to_f
 d = b**2 - (4 * a * c)
 
 if d == 0
-	x = -b / (2 * a)
-	puts "The equation has one root which is equal #{x}."
+  x = -b / (2 * a)
+  puts "The equation has one root which is equal #{x}."
 elsif d > 0
-	root = Math.sqrt(d)
-	x1 = (-b + root) / (2 * a)
-	x2 = (-b - root) / (2 * a)
-	puts "The equation has two roots which are equal #{x1} and #{x2} respectively."
+  root = Math.sqrt(d)
+  x1 = (-b + root) / (2 * a)
+  x2 = (-b - root) / (2 * a)
+  puts "The equation has two roots which are equal #{x1} and #{x2} respectively."
 else
-	puts "There are no roots."
+  puts "There are no roots."
 end
