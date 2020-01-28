26bf6605e122ed9539f50c852b230f5991be57de
kahahope <kahahope@gmail.com>
2020-01-28 04:23:48 +0300

    Tab changes

--- Lesson1/ideal_weight.rb
+++ Lesson1/ideal_weight.rb
@@ -7,7 +7,7 @@ height = gets.to_i
 ideal_weight = (height - 110) * 1.15
 
 if ideal_weight >= 0
-	puts "#{name}, your ideal_weight is #{ideal_weight} kg."
+  puts "#{name}, your ideal_weight is #{ideal_weight} kg."
 else
-	puts "#{name}, your weight is ideal already!"
+  puts "#{name}, your weight is ideal already!"
 end
